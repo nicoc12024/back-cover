4490ab78fd0c5e7e422adb3b276aa7846840c3e5
Nicolas <56944680+nicoc12024@users.noreply.github.com>
2023-03-19 19:34:22 +0100

    m

--- backend/settings.py
+++ backend/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': dj_database_url.config(default='postgres://user:password@localhost/dbname')
 }
 
 # Password validation
